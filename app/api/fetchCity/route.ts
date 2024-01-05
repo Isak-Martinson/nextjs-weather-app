ac40997275261de681855dcfb917d52cda70641a
Isak Martinson <isak.martinson@iths.se>
2024-01-05 19:55:42 +0100

    updated the api route, so it fetches the baseUrl based on search params and after that the weather API based on latitude and longitude

--- app/api/fetchCity/route.ts
+++ app/api/fetchCity/route.ts
@@ -1,11 +1,18 @@
 import { apiKey } from '@/config';
+import { NextApiRequest } from 'next';
+import { NextRequest } from 'next/server';
 
-export async function GET(req: string) {
+export async function GET(req: NextRequest) {
   console.log('api route hit');
-  console.log('request: ', req);
-  const baseUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${'gothenburg'}&limit=&appid=${apiKey}`;
+  const city = req.nextUrl.searchParams.get('city');
+  const baseUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=&appid=${apiKey}`;
   const response = await fetch(baseUrl);
   const data = await response.json();
-  console.log(JSON.stringify(data));
-  return new Response(JSON.stringify(data));
+  const lat = JSON.stringify(data[0].lat);
+  const lon = JSON.stringify(data[0].lon);
+  console.log(lat, lon);
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  const responseTwo = await fetch(weatherUrl);
+  const dataTwo = await responseTwo.json();
+  return new Response(JSON.stringify(dataTwo));
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,33 @@
 import styles from './page.module.css';
 import { useEffect, useState } from 'react';
 import SearchComponent from './components/SearchComponent';
+import ForecastComponent from './components/ForecastComponent';
 
 export default function Home() {
-  const [lat, setLat] = useState('');
-  const [lon, setLon] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  useEffect(() => {
+    console.log('useEffect weather data: ', weatherData);
+  }, [weatherData]);
   const makeApiCall = async (city: any) => {
-    const response = await fetch('/api/fetchCity', {
-      method: 'GET',
-    });
+    const response = await fetch(
+      `/api/fetchCity?city=${encodeURIComponent(city)}`,
+      {
+        method: 'GET',
+      }
+    );
     const data = await response.json();
-    console.log('city: ', city, 'fetched data', data[0]);
-    setLat(JSON.stringify(data[0].lat));
-    setLon(JSON.stringify(data[0].lat));
-    console.log(lat, lon);
+    console.log('city: ', city, 'fetched data', data);
+    setWeatherData(data);
   };
 
   return (
     <main className={styles.main}>
       <SearchComponent handleApiCall={makeApiCall} />
-      <h1>Weather App</h1>
-      <button onClick={makeApiCall}>api call</button>
+      {weatherData !== null ? (
+        <ForecastComponent weatherData={weatherData} />
+      ) : (
+        <p>Search by any city in the searchbar</p>
+      )}
     </main>
   );
 }
